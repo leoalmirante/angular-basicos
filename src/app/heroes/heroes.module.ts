e40ad13cf8e39a3ee28c367b478ed577bb0e34e9
Jesús Almirante Laurente <jalmirante@hardata.cl>
2022-02-22 23:49:12 -0300

    primer commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ListadoComponent } from './listado/listado.component';
+import { HeroeComponent } from './heroe/heroe.component';
+
+
+@NgModule({
+    declarations: [
+        HeroeComponent,
+        ListadoComponent
+    ],
+    exports: [
+        ListadoComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class HeroesModule {}
